9087e9cdaf9748cd316811e58e47f6ac5d642c28
Rashid khan <rashidkhanbd57@gmail.com>
2022-05-02 01:14:49 +0600

    server ready for delete update add

--- index.js
+++ index.js
@@ -43,6 +43,25 @@ async function run() {
       const result = await productsCollection.insertOne(data);
       res.send(result);
     })
+    // delete items 
+    app.delete('/products/:id',async(req , res) => {
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+    // for update 
+    app.put('/products/:id',async(req,res )=>{
+      const id =req.params.id;
+      const user = req.body;
+      const query = {_id:ObjectId(id)};
+      const options = { upsert: true };
+      const update = {
+        $set:user
+      }
+      const result = await productsCollection.updateOne(query,update,options);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
