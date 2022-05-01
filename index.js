4f37ab4d4832712b14fae4327de1321f8d7bd529
Rashid khan <rashidkhanbd57@gmail.com>
2022-05-01 12:34:10 +0600

    same problem fetch

--- index.js
+++ index.js
@@ -30,13 +30,7 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result)
     });
-    // get single products 
-    app.get('/product/:id',async(req , res )=>{
-      const id = req.params.id;
-      const query = {_id:ObjectId(id)}
-      const result = await productsCollection.findOne(query);
-      res.send(result)
-    })
+    
   } finally {
     // await client.close();
   }
